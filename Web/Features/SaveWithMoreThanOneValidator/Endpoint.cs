90cabd691d88e085fe9597345d3b9adde21eba5f
Jesus <wjaxxx@gmail.com>
2022-05-27 00:17:43 +0200

    Testing for nested request and several custom user validators per class

---
+++ Web/Features/SaveWithMoreThanOneValidator/Endpoint.cs
@@ -0,0 +1,57 @@
+﻿namespace Web.Features.SaveWithMoreThanOneValidator;
+
+public class SaveWithMoreThanOneValidator : Endpoint<Request>
+{
+    public override void Configure()
+    {
+        Put("Test/SaveWithMoreThanOneValidator");
+        AllowAnonymous();
+        Summary(summary => summary.Description = $"This is testing '{nameof(SaveWithMoreThanOneValidator)}'");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        await SendOkAsync(ct);
+    }
+}
+
+public class RequestChild
+{
+    public Guid? NullableIdentifier { get; set; }
+    public string? Message { get; set; }
+}
+
+public class Request
+{
+    public Guid Identifier { get; set; }
+    public RequestChild? RequestChild { get; set; }
+}
+
+public class RequestValidator : Validator<Request>
+{
+    public RequestValidator()
+    {
+        RuleFor(x => x.Identifier).NotEmpty();
+    }
+}
+
+public class OtherUserClass
+{
+    public string message { get; set; }
+}
+
+public class OtherUserClassValidatorOne : AbstractValidator<OtherUserClass>
+{
+    public OtherUserClassValidatorOne()
+    {
+        RuleFor(x => x.message).NotEmpty();
+    }
+}
+
+public class OtherUserClassValidatorTwo : AbstractValidator<OtherUserClass>
+{
+    public OtherUserClassValidatorTwo()
+    {
+        RuleFor(x => x.message).NotNull();
+    }
+}
---
+++ Web/Features/SaveWithNestedRequest/SaveWithNestedRequest.cs
@@ -0,0 +1,37 @@
+﻿// namespace Web.Features.SaveWithNestedRequest;
+//
+// public class SaveWithNestedRequest  : Endpoint<Request>
+// {
+//     public override void Configure()
+//     {
+//         Put("Test/SaveWithNestedRequest");
+//         AllowAnonymous();
+//         Summary(summary => summary.Description = $"This is testing {nameof(SaveWithNestedRequest)}'");
+//     }
+//
+//     public override async Task HandleAsync(Request req, CancellationToken ct)
+//     {
+//         await SendOkAsync(ct);
+//     }
+// }
+//
+// public class Request
+// {
+//     public string? Message { get; set; }
+//     public NestedClass nestedParent { get; set; }
+// }
+//
+// public class NestedClass
+// {
+//     public string Identifier { get; set; }
+//     public NestedClass nestedChild { get; set; } 
+// }
+//
+// public class RequestValidator : Validator<Request>
+// {
+//     public RequestValidator() : base()
+//     {
+//         RuleFor(x => x.Message).Length(3, 100);
+//         RuleFor(x => x.nestedParent.Identifier).Length(4, 100);
+//     }
+// }
